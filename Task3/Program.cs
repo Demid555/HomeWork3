c20e10311e6bbe1db998c3146f99aae9eaa8186d
Demid Kolesnikov <Demidcol@yandex.ru>
2023-12-27 10:00:06 +0300

    Третье задание

---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+
+void PrintArray(int[] array, int i = 0)
+
+{
+    if (i == array.Length) return;
+    PrintArray(array, i + 1);
+    Console.Write(Convert.ToInt32(array[i]));
+    Console.Write(" ");
+}
+
+
+
+int[] myArray = {2 , 4 , 53, 6, 7};
+PrintArray(myArray, 0);
